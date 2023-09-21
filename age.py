ef3c8e613edca95dc7bec796799bc5961d87aa47
Hafiz Umair Ijaz <hafizumairijaz@gmail.com>
2023-09-21 12:28:00 +0500

    basic conditonal statement

---
+++ age.py
@@ -0,0 +1,15 @@
+age=int(input('enter a age '))
+
+if age >18   and age <= 65 :
+    print('you are an adult ')
+if age > 65 :
+    print(' yor are a senoir citizen')
+if age < 18:
+
+    print('you are a minor ')
+
+
+
+
+
+
